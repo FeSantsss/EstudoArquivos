cad3f09b8911b8e1309cf7bc4da39ba8d36d28c9
Felipy <tercetime18@gmail.com>
2025-06-08 23:37:06 -0300

    add estudo 01

---
+++ src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			File file = new File("/home/santsss/Documentos/in.txt");
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+
+			String line = br.readLine();
+
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			try {
+				if (fr != null) {
+					fr.close();
+				}
+			} catch (IOException e) {
+				System.out.println("Error closing FileReader: " + e.getMessage());
+			}
+		}
+
+	}
+
+}
