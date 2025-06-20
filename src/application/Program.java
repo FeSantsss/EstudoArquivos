0e4cba2cb1996340d4716d998f5bea6fd9b5720d
Felipy <tercetime18@gmail.com>
2025-06-20 19:42:54 -0300

    Exercicio proposto: Fazer um programa para ler o caminho de um arquivo .csv contendo os dados de itens vendidos. Cada item possui um nome, preço unitário e quantidade, separados por vírgula. Você deve gerar um novo arquivo chamado summary.csv, localizado em uma subpasta chamada out a partir da pasta original do arquivo de origem, contendo apenas o nome e o valor total para aquele item (preço unitário multiplicado pela quantidade)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,31 +1,68 @@
 package application;
 
+import model.entities.Product;
+
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter folder path:");
-		String strPath = sc.nextLine();
-
-		File path = new File(strPath);
-
-		File[] folders = path.listFiles(File::isDirectory);
-
-		System.out.println("FOLDERS:");
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
+		List<Product> list = new ArrayList<>();
 		
-		File[] files = path.listFiles(File::isFile);
-		if (files != null) {
-			System.out.println("FILES:");
-			for (File file : files) {
-				System.out.println(file);
+		System.out.println("Adicione o caminho do arquivo:");
+		String pathStr = sc.nextLine();
+		File path = new File(pathStr);
+		String pathParent = path.getParent();
+		
+		Boolean exists = new File(pathParent + "/out").mkdir();
+		
+		String outPath = pathParent + "/out/summary.csv";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			String line = br.readLine();
+			while(line != null) {
+				
+				String[] fields = line.split(",");
+				String name = fields[0];
+				Double price = Double.parseDouble(fields[1]);
+				Integer quant = Integer.parseInt(fields[2]);
+				Product product = new Product(name, price, quant);
+				
+				list.add(product);
+				
+				line = br.readLine();
 			}
+			
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(outPath))) {
+				for(Product p : list) {
+					bw.write(p.toString());
+					bw.newLine();
+				}
+				
+				System.out.println("Arquivo criado: " + outPath);
+			} catch(IOException e) {
+				System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
+				
+			}
+			
+			System.out.println();
+			
+			try (BufferedReader brFinally = new BufferedReader(new FileReader(outPath))) {
+				String lineOut = brFinally.readLine();
+				while(lineOut != null) {
+					System.out.println(lineOut);
+					lineOut = brFinally.readLine();
+				}
+			} catch(IOException e) {
+				System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+			}
+			
+		} catch(Exception e) {
+			System.out.println("Erro: " + e.getMessage());
 		}
 	}
 
